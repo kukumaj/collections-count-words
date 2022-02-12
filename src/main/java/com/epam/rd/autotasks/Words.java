8721ba0abb474a3024a830ab4eb4e7da6a254137
Ryszard <52924153+kukumaj@users.noreply.github.com>
2022-02-12 19:30:19 +0100

    additional complexity changes

--- src/main/java/com/epam/rd/autotasks/Words.java
+++ src/main/java/com/epam/rd/autotasks/Words.java
@@ -13,17 +13,14 @@ public class Words {
 
             String[] arr = line.toLowerCase().replaceAll("[^0-9a-zA-Z\u0430-\u044F\u0410-\u042F\u0100-\u017F\u0180-\u024F\u0080-\u00FF]", " ").trim().split("\\s");
 
-
             for (String word : arr) {
-                if (!map.containsKey(word) && word.length() >= 4) {
+                if (word.length() < 4) continue;
+                if (!map.containsKey(word)) {
                     map.put(word, 0);
                 }
-                if (word.length() >= 4) {
-                    map.put(word, map.get(word) + 1);
-                }
+                map.put(word, map.get(word) + 1);
             }
         }
-
 
         List<WordsStats> list = new ArrayList<>(map.size());
         for (Map.Entry<String, Integer> entry : map.entrySet()) {
@@ -33,16 +30,15 @@ public class Words {
         list.sort(new Comparator<>() {
             @Override
             public int compare(WordsStats o1, WordsStats o2) {
-                if (o1.getOccurrences() > o2.getOccurrences()) {
-                    return -1;
-                } else if (o1.getOccurrences() < o2.getOccurrences()) {
-                    return 1;
-                } else {
+
+                if (o1.getOccurrences() == o2.getOccurrences()) {
                     return o1.getWord().compareTo(o2.getWord());
+                } else {
+                    return o1.getOccurrences() < o2.getOccurrences() ? 1 : -1;
                 }
+
             }
         });
-
 
         StringBuilder result = new StringBuilder();
 
